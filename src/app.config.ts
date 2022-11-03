b2997bcd68def67e9602d1ba8898402ad9d757b5
unfetteredman <weixw2014@qq.com>
2022-11-03 15:55:42 +0800

    add: 添加首页和个人中心

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,31 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
+  pages: ['pages/home/index', 'pages/usercenter/index'],
+  tabBar: {
+    custom: true,
+    color: '#000000',
+    selectedColor: '#000000',
+    backgroundColor: '#000000',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/usercenter/index',
+        text: '个人中心',
+      },
+    ],
+  },
+  subpackages: [
+    {
+      root: 'packages/user',
+      pages: ['index'],
+    },
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTextStyle: 'black',
+  },
+});
---
+++ src/pages/usercenter/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserCenter() {
+  return (
+    <div>index</div>
+  )
+}
